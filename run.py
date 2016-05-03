86ec3db29c2a2d41dd8defd2faa56a3957cb8e91
XzAmrzs <pcxzp@live.com>
2016-05-03 20:01:21 +0800

    first test

---
+++ run.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import time  
+from flask import Flask,request, make_response  
+import hashlib  
+
+app=Flask(__name__)
+     
+@app.route('/', methods = ['GET', 'POST'] )  
+def wechat_auth():
+    if request.method == 'GET':
+        token = 'test' # your token  
+        query = request.args  # GET 方法附上的参数  
+        signature = query.get('signature', '')  
+        timestamp = query.get('timestamp', '')  
+        nonce = query.get('nonce', '')  
+        echostr = query.get('echostr', '')  
+        s = [timestamp, nonce, token]  
+        s.sort()  
+        s = ''.join(s)  
+        if ( hashlib.sha1(s).hexdigest() == signature ):    
+          return make_response(echostr)  
+
+if __name__ =='__main__':
+    app.run()
